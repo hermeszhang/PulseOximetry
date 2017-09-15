61afb8f2a37d80bd7c64e9bd6ab1f4801f548b6a
Jules Calella <julescalella@gmail.com>
2017-09-15 20:23:53 +0000

    Added i2c Library.

--- main.c
+++ main.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include "pulseOx.h"
+#include "myi2clib.h"
 
 int main(void)
 {
 	printf("Pulse Oximetry Reader\n\n");
 
-	pulseOxSetup();
+//	pulseOxSetup();
 
-	pulseOxRead();
+//	pulseOxRead();
+
+	i2cSetup();
+	uint8_t rv;
+	rv = i2cWrite(0xFF, 0x00);
+	printf("rv = %d \n", rv);
+
+	rv = i2cWrite(0xFF, 0xFF);
+	printf("rv = %d \n", rv);
+
+	rv = i2cWrite(0xFF, 0xAE);
+	printf("rv = %d \n", rv);
+
+	rv = i2cWrite(0xFF, 0xFF);
+	printf("rv = %d \n", rv);
+
 
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 CC=gcc
 CFLAGS= -I. -lwiringPi
-DEPS= pulseOx.h 
+DEPS= pulseOx.h myi2clib.h
 
 %.o: %.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
 
-PulseOximetry: main.o pulseOx.o
-	$(CC) -o PulseOximetry main.o pulseOx.o $(CFLAGS)
+PulseOximetry: main.o pulseOx.o myi2clib.o
+	$(CC) -o PulseOximetry main.o pulseOx.o myi2clib.o $(CFLAGS)
